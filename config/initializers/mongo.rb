46d432629b955bc7fe9d5df1dd248384adf91497
TrimbleTodd <toddmosier@gmail.com>
2011-07-02 22:54:54 -0400

    db fun

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,9 +1,14 @@
-puts "In mongo.rb initializer"
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "hhh_#{Rails.env}"
+puts "In mongo.rb initializer in #{Rails.env}"
+yaml_config = YAML.load_file("config/mongo.yml")[Rails.env]
 
-puts "#hhh_#{Rails.env}"
-puts MongoMapper.database
+puts yaml_config
+
+MongoMapper.connection = Mongo::Connection.new(yaml_config["host"], yaml_config["port"])
+MongoMapper.database = yaml_config["database"]
+# MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# MongoMapper.database = "hhh_#{Rails.env}"
+
+puts "Loading MongoDB #{MongoMapper.database} (#{yaml_config["database"]}) via MongoMapper"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
